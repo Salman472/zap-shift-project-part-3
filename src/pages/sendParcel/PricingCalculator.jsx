0bc2b4f9dedda3256a6680b8eb6aaaa8da25515c
Salman472 <mds619820@gmail.com>
2026-03-30 22:12:34 +0600

    calculate parcel send cost

--- src/pages/sendParcel/PricingCalculator.jsx
+++ src/pages/sendParcel/PricingCalculator.jsx
@@ -22,10 +22,30 @@ const PricingCalculatorModal = ({ isOpen, onClose,serviceCenters }) => {
     const districts = regionDistrict.map((d) => d.district);
     return districts;
   };
-  console.log(districtByRegion);
-  const handleCalculate=(data)=>{
+  // console.log(districtByRegion);
+ const handleCalculate = (data) => {
     console.log(data);
-  }
+    // calculate delivery charge
+   const isDocument = data.parcelType === 'document'
+   const isSameDistrict= data.senderDistrict === data.receverDistrict
+   const parcelWeight=parseFloat(data.parcelWeight)
+   let cost = 0
+    if(isDocument){
+      cost = isSameDistrict ? 60 : 80
+    }
+    else{
+      if(parcelWeight <= 3){
+        cost = isSameDistrict ? 110 : 150
+      }
+      else{
+        const minCharge = isSameDistrict ? 110 : 150
+        const extraWeight = parcelWeight - 3
+        const extraCharge = isSameDistrict ? extraWeight * 40 : extraWeight * 40 + 40
+        cost = minCharge + extraCharge
+      }
+    }
+    console.log(cost);
+  };
   if (!isOpen) return null;
 
   return (
